10422f5d66ae48e52c55ef5f72490e9e8eefa14a
aliciariveraa <aliciarivera@usf.edu>
2025-03-16 16:59:16 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,6 @@
+// Task 1: Base structure setup
+
+document.addEventListener("DOMContentLoaded", () => {
+    const riskDashboard = document.getElementById("riskDashboard");
+    console.log("Risk Dashboard Loaded");
+});
